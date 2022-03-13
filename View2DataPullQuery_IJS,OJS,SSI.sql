5ef443093836d6f0f8984f475c50a93ebdd8e713
Shivani <46258102+shivi1019@users.noreply.github.com>
2022-03-13 16:00:07 -0700

    Update View2DataPullQuery_IJS,OJS,SSI.sql

--- View2DataPullQuery_IJS,OJS,SSI.sql
+++ View2DataPullQuery_IJS,OJS,SSI.sql
@@ -1,4 +1,3 @@
--- SSI WEEK LEVEL IJS/OJS order count, incident count -- 
 
 -- CODE STARTS HERE -- 
 
@@ -9,11 +8,7 @@ select
 	,P90_IJS
 	,P90_OJS
 	,round(  sum(cnt_inc_id) / count( distinct order_external_id) * 100) as Incident_to_order
-	,ssi_column
-	,issue_type	
-	,sub_issue_type	
-	,sub_sub_issue_type
-					
+	,ssi_column				
 	
 	from 
 		(select 
@@ -23,39 +18,30 @@ select
 			,cnt_inc_id
 			,percentile(cast(total_ijs_score as bigint),0.9) OVER (PARTITION BY ( ssi_column)) as P90_OJS	
 			,ssi_column
-			,issue_type	
-			,sub_issue_type	
-			,sub_sub_issue_type
 				
 		from 
 			(
 			select 
 				order_external_id
-				,sum(incident_ijs_score) as total_ijs_score
+				,sum(score) as total_ijs_score
 				,P90_IJS
 				,cnt_inc_id
 				,ssi_column
-				,issue_type	
-				,sub_issue_type	
-				,sub_sub_issue_type
-					
+						
 			from
 				(select  
 					distinct 
 					order_external_id
 					,incident_id
-					,incident_ijs_score 
-					,percentile(cast(incident_ijs_score as bigint),0.9) OVER (PARTITION BY ( concat(issue_type, sub_issue_type, sub_sub_issue_type))) as P90_IJS		
+					,score 
+					,percentile(cast(incident_ijs_score as bigint),0.9) OVER (PARTITION BY ( concat(issue, sub_issue))) as P90_IJS		
 					,weekofyear(to_date(incident_creation_date)) as incident_week
 					,to_date(incident_creation_date) as incident_creation_date
-					,issue_type	
-					,sub_issue_type	
-					,sub_sub_issue_type
-					,concat(issue_type, sub_issue_type, sub_sub_issue_type) as ssi_column
+					,concat(issue, sub_issue) as ssi_column
 					,count ( distinct incident_id) over ( partition by order_external_id) as cnt_inc_id
 					
 				from 
-					bigfoot_external_neo.mp_cs__ims_incident_hive_fact
+					incident_table
 				where 
 					weekofyear(to_date(incident_creation_date))  = 2
 				and 
@@ -67,9 +53,6 @@ select
 			,order_external_id
 			,cnt_inc_id
 			,ssi_column
-			,issue_type	
-			,sub_issue_type	
-			,sub_sub_issue_type
 					
 			) b
 		)c
@@ -77,7 +60,4 @@ group by
 P90_IJS
 ,P90_OJS 
 ,ssi_column
-,issue_type	
-,sub_issue_type	
-,sub_sub_issue_type
 					
